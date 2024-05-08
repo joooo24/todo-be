69d9a9da787a18c74fc0df51f17dfe04c8563e95
주현정 <61533589+joooo24@users.noreply.github.com>
2024-05-08 11:41:38 +0900

    chore: mongoose, bodyParser 설정

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json())
+
+// MongoDB 연결
+const mongoURI = `mongodb://localhost:27017/todo-demo`
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('MongoDB connected');
+    })
+    .catch((err) => {
+        console.error('MongoDB connection error:', err);
+    });
+
 
 // "/" 경로에 대한 GET 요청을 처리하는 미들웨어를 추가
 app.get('/', (req, res) => {
