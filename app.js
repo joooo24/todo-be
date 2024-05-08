42fc3b0ee9bc027d4448cc784f223abdcd2938d0
주현정 <61533589+joooo24@users.noreply.github.com>
2024-05-08 16:22:54 +0900

    chore: 포트번호 수정

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.get('/', (req, res) => {
 });
 
 // Express 애플리케이션이 리스닝할 포트를 설정하고 서버를 시작
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
