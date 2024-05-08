e96cf6bb1d3d4fcd409c567568bb7ec55eae806d
주현정 <61533589+joooo24@users.noreply.github.com>
2024-05-08 15:35:27 +0900

    test: test 코드 작성

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const indexRounter = require("./routes/index")
+const indexRounter = require("./routes/index");
+const Task = require('./models/task');
 
 // bodyParser, indexRounter 사용
 const app = express();
@@ -13,7 +14,30 @@ const mongoURI = `mongodb://127.0.0.1:27017/todo-demo`
 mongoose.connect(mongoURI, { useNewUrlParser: true })
     .then(() => {
         console.log('MongoDB connected');
+
+        // test:
+        // 새로운 할일 생성
+        const newTask = new Task({
+            task: '새로운 할일 생성', // 새로운 할일의 내용
+            isComplete: false // 완료 여부 설정 (기본값: false)
+        });
+
+        // 할일 저장
+        return newTask.save() // 새로운 할일을 데이터베이스에 저장하고 Promise 반환
+            .then((savedTask) => { // 저장된 할일 정보를 받아서 처리하는 부분
+                console.log('새로운 할일 저장되었습니다.');
+                console.log(savedTask);
+
+                // 저장된 할일 검색
+                return Task.find({}); // 저장된 모든 할일을 검색하고 Promise 반환
+            })
+            .then((tasks) => { // 검색된 할일 목록을 받아서 처리하는 부분
+                console.log('저장된 할일 목록:');
+                console.log(tasks);
+            });
     })
+
+
     .catch((err) => {
         console.error('MongoDB connection error:', err);
     });
@@ -21,7 +45,7 @@ mongoose.connect(mongoURI, { useNewUrlParser: true })
 
 // "/" 경로에 대한 GET 요청을 처리하는 미들웨어를 추가
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+    res.send('Hello, world!'); // 브라우저에서 루트 URL에 접속하면 'Hello, world!' 텍스트를 반환
 });
 
 // Express 애플리케이션이 리스닝할 포트를 설정하고 서버를 시작
