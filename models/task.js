ad057d3a75122ebedccd43738e10af09ff595476
주현정 <61533589+joooo24@users.noreply.github.com>
2024-05-08 13:53:26 +0900

    feat: model task 스키마 정의

---
+++ models/task.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const taskSchema = mongoose.Schema({
+    task: {
+        type: String,
+        required: true
+    },
+    isComplete: {
+        type: Boolean,
+        required: true
+    }
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
