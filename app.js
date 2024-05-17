1f76ecfed44328cf6d35626cd034a4c164cebe76
주현정 <61533589+joooo24@users.noreply.github.com>
2024-05-17 14:50:26 +0900

    Update app.js

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const app = express();
 require('dotenv').config();
 const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD
 
-// bodyParser, indexRounter, cors 사용
+// bodyParser, indexRounter, cors 설정
 app.use(bodyParser.json());
 app.use("/api", indexRounter);
-app.use(cors())
+app.use(cors());
 
 // MongoDB 연결
 const mongoURI = MONGODB_URI_PROD
@@ -46,13 +46,12 @@ mongoose.connect(mongoURI, { useNewUrlParser: true })
         console.error('MongoDB connection error:', err);
     });
 
-
-// "/" 경로에 대한 GET 요청을 처리하는 미들웨어를 추가
+// 기본 경로 설정 - "/" 경로에 대한 GET 요청을 처리하는 미들웨어를 추가
 app.get('/', (req, res) => {
-    res.send('Hello, world!'); // 브라우저에서 루트 URL에 접속하면 'Hello, world!' 텍스트를 반환
+    res.send('Hello, world!');
 });
 
-// Express 애플리케이션이 리스닝할 포트를 설정하고 서버를 시작
+// 서버 시작 - Express 애플리케이션이 리스닝할 포트를 설정하고 서버를 시작
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
