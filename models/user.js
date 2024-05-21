faae4dcb1465007a76da24d99ecd837cc710e339
주현정 <61533589+joooo24@users.noreply.github.com>
2024-05-21 17:15:08 +0900

    chore: JWT_SECRET_KEY 네이밍 변경

--- models/user.js
+++ models/user.js
@@ -1,38 +1,38 @@
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
-require('dotenv').config()
-const JWT_SECRETE_KEY = process.env.JWT_SECRETE_KEY
+require('dotenv').config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
-const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true
+const userSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        }
     },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
-},
-    { timestamps: true });
+    { timestamps: true }
+);
 
 // 토큰 생성
 userSchema.methods.generateToken = function () {
-    const token = jwt.sign({ _id: this._id }, JWT_SECRETE_KEY, { expiresIn: "1h" });
+    const token = jwt.sign({ _id: this._id }, JWT_SECRET_KEY, { expiresIn: '1h' });
     return token;
 };
 
 // 비밀번호 제거
 userSchema.methods.toJSON = function () {
     const obj = this._doc;
-    delete obj.password
-    return obj
-}
+    delete obj.password;
+    return obj;
+};
 
 const User = mongoose.model('user', userSchema);
 module.exports = User;
-
-
